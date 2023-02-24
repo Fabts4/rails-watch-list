ebb1229d1493fa56ce3484530ba0fd44cacb4bed
Fabien James <fabts4@gmail.com>
2023-02-24 22:02:32 +0100

    All done

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,8 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmarks = Bookmark.where(list_id: params[:id])
+    @review = Review.new
+    @reviews = @list.reviews
   end
 
   def new
@@ -24,6 +26,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,8 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
   has_many :reviews
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
